163f6da2152013ef37905ac0b1d602c54a212eb2
aleksandra-razumova <air.scorp@yandex.ru>
2023-10-12 17:25:45 +0300

    w/ comments

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,20 +7,24 @@ import org.jsoup.select.Elements;
 public class Main {
 
     public static void main(String[] args) throws Exception {
+        //Получаем объект Document, представление HTML страницы.
         Document page = Parser.getPage();
-        //css query language
+        //Получаем объект Element, выделяем нужную таблицу, в которой представлена информация по погоде.
         Element tableWeather = page.select("table[class=wt]").first();
         assert tableWeather != null;
+        //Получаем из общей таблицы даты
         Elements names = tableWeather.select("tr[class=wth]");
+        //Получаем из общей таблицы значения погоды
         Elements values = tableWeather.select("tr[valign=top]");
-        int index = 1;
-        for (Element name : names) {
-            String dateString = name.select("th[id=dt]").text();
-            String date = Parser.getDateFromString(dateString);
-            System.out.printf("%-10s %-70s %-15s %-10s %-15s %-15s",date, "Явления","Температура","Давление",
-                    "Влажность","Ветер");
-            System.out.println();
+        int index = 0;
 
+        //Вывод погоды на экран на ближайшие 5 дней
+        for (Element name : names) {
+            String dateString = name.select("th[id=dt]").text(); //Получаем текстовое значение заголовка
+            String date = Parser.getDateFromString(dateString);  //Переводим заданный формат даты
+            System.out.printf("%-10s %-70s %-15s %-10s %-15s %-15s",date, "Явления","Температура","Давление",
+                    "Влажность","Ветер"); //Печатаем отформатированную шапку таблицы
+            System.out.println();
             index += Parser.printFourValues(values, index);
         }
     }
--- src/main/java/org/example/Parser.java
+++ src/main/java/org/example/Parser.java
@@ -12,12 +12,15 @@ import java.util.regex.Pattern;
 
 public class Parser {
     static Document getPage() throws IOException {
+        //Получаем объект Document, представление HTML страницы.
         String url = "https://www.pogoda.msk.ru";
         return Jsoup.parse(new URL(url), 3000);
     }
 
+    //Чтобы дата выводилась определенного формата без лишней информации, создаем паттерн.
     private static final Pattern pattern = Pattern.compile("\\d{2}\\.\\d{2}");
     static String getDateFromString(String stringDate) throws Exception {
+        //Получаем дату необходимого формата из строки.
         Matcher matcher = pattern.matcher(stringDate);
         if (matcher.find()){
             return matcher.group();
@@ -26,22 +29,26 @@ public class Parser {
     }
 
     static int printFourValues(Elements values, int index) {
+        //Выводим на печать информацию по погоде за день (Утро, день, вечер, ночь).
         int iterationCount = 4;
 
         if (index==0) {
-            Element valueLn = values.get(1);
+            Element valueLn = values.get(0);
+            /*Т.к. представление погоды меняется на сайте в зависимости от времени, проверяем воспадение первого
+            элемента, для корректного вывода на оставшиеся сутки. */
             if (valueLn.text().contains("День")){
                 iterationCount = 3;
             } else if (valueLn.text().contains("Вечер")){
-                iterationCount=2;
+                iterationCount = 2;
             } else if (valueLn.text().contains("Ночь")){
-                iterationCount=1;
+                iterationCount = 1;
             }
         }
 
         for (int i = 0; i < iterationCount; i++) {
             Element valueLine = values.get(index + i);
             int j=0;
+            //Получаем нужный тег и выводим на экран с добавлением форматирования, чтобы информация была читабельной.
             for (Element td : valueLine.select("td")) {
                 switch (j) {
                     case (0):
@@ -56,7 +63,6 @@ public class Parser {
                         j++;
                         break;
                     case (2):
-
                         System.out.printf("%-20s", td.text());
                         j++;
                         break;
